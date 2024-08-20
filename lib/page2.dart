be0ac1fdc4fc1d673f501af18495a08fce1dd11d
ajengvz <ajgvaniamarhas@gmail.com>
2024-08-20 11:53:42 +0700

    pesan commit atau perubahannya

---
+++ lib/page2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/page2');
+          },
+          child: Text('pindah ke halaman 2'),
+          ),
+      ),
+    );
+  }
+}
